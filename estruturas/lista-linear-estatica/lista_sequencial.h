2cfba817d5de9c2888bf72f9e32ad000676ae12c
Gabriel <61798393+gjperes@users.noreply.github.com>
2022-02-11 16:38:47 -0300

    melhorados comentários das funções implementadas

--- estruturas/lista-linear-estatica/lista_sequencial.h
+++ estruturas/lista-linear-estatica/lista_sequencial.h
@@ -5,22 +5,41 @@ typedef struct Vetor Tipo_Lista;
 #define MAX_VETOR 5
 
 // TODO: Melhorar a descrição das funções
-// Cria uma lista estática
+
+/** Cria uma nova lista e aloca ela em memória
+ *  @return Retorna a nova lista
+ */
 Tipo_Lista* criarLista();
 
-// Desaloca de memória a lista estática
+/** Desaloca de memória a lista estática
+ * @param lista Lista que será desalocada
+ */
 void liberarLista(Tipo_Lista *lista);
 
-// Retorna false só se a lista está vazia (sem dados)
+/** Verifica se a lista está cheia
+ * @param lista Lista para verificar
+ * @return Lista Cheia ? True : False *Se NULL retorna -1
+ */
 int verificarListaCheia(Tipo_Lista *lista);
 
-// Retorna false só se a lista tem algum dado
+/** Verifica se a lista está vazia
+ * @param lista Lista para verificar
+ * @return Lista Vazia ? True : False *Se NULL retorna -1
+ */
 int verificarListaVazia(Tipo_Lista *lista);
 
-// Insere o dado no ínicio da lista
+/** Insere o dado no início da lista (posição 0)
+ * @param lista Lista para verificar
+ * @param valor Valor a ser inserido na lista
+ * @return Sucesso na operação ? True : False
+ */
 int inserirInicioLista(Tipo_Lista *lista, int valor);
 
-// Insere o dado no fim da lista
+/** Insere o dado no final da lista
+ * @param lista Lista para verificar
+ * @param valor Valor a ser inserido na lista
+ * @return Sucesso na operação ? True : False
+ */
 int inserirFinalLista(Tipo_Lista *lista, int valor);
 
 int inserirMeioLista(Tipo_Lista *lista, Tipo_Lista *novoNode, int posicao);
