07f4bc3373eafa6dac9b15fdc0eee3ef285a76a8
Gabriel <61798393+gjperes@users.noreply.github.com>
2022-02-11 16:27:13 -0300

    implementadas funções de criar/verificar

--- estruturas/lista-linear-estatica/lista_sequencial.c
+++ estruturas/lista-linear-estatica/lista_sequencial.c
@@ -4,7 +4,7 @@
 
 struct Vetor {
   int dados[MAX_VETOR];
-  int inicio, fim;
+  int fim;
 };
 
 Tipo_Lista* criarLista() {
@@ -16,4 +16,60 @@ Tipo_Lista* criarLista() {
     novaLista->fim = 0;
 
   return novaLista;
+}
+
+void liberarLista(Tipo_Lista *lista) {
+  free(lista);
+}
+
+int verificarListaCheia(Tipo_Lista *lista) {
+  if(lista == NULL) 
+    return (-1);
+
+  return (lista->fim == MAX_VETOR);
+}
+
+int verificarListaVazia(Tipo_Lista *lista) {
+  if(lista == NULL)
+    return (-1);
+
+  return (lista->fim == 0);
+}
+
+int inserirInicioLista(Tipo_Lista *lista, int valor) {
+  if(lista == NULL) return 0;
+  if(verificarListaCheia(lista)) return 0;
+  
+  if(!verificarListaVazia(lista)) {
+    for(int i = (lista->fim - 1); i >= 0; i--) {
+      lista->dados[i + 1] = lista->dados[i];
+    }
+  }
+
+  lista->dados[0] = valor;
+  lista->fim++;
+
+  return 1;
+}
+
+int inserirFinalLista(Tipo_Lista *lista, int valor) {
+  if(lista == NULL) return 0;
+  if(verificarListaCheia(lista)) return 0;
+  
+  lista->dados[lista->fim] = valor;
+  lista->fim++;
+
+  return 1;
+}
+
+// TODO: Implementar a inserção no meio (ordenado)
+
+// TODO: Precisa ser concluído
+int removerInicioLista(Tipo_Lista *lista) {
+  if(lista == NULL) return 0;
+  if(verificarListaVazia) return 0;
+
+  lista->dados[0] = 0;
+
+  return 1;
 }
--- estruturas/lista-linear-estatica/lista_sequencial.h
+++ estruturas/lista-linear-estatica/lista_sequencial.h
@@ -4,18 +4,24 @@ typedef struct Vetor Tipo_Lista;
 // Tamanho máximo de valores que podem ser armazenados em dado da TAD
 #define MAX_VETOR 5
 
+// TODO: Melhorar a descrição das funções
 // Cria uma lista estática
-Tipo_Lista* criarLista(int dados[]);
+Tipo_Lista* criarLista();
+
 // Desaloca de memória a lista estática
-int liberarLista(Tipo_Lista *lista);
-// Verifica se a lista está com o vetor de dados cheio
-int verificarListaCheia(Tipo_Lista *lista, int tamanhoMax);
-// Verifica se a lista está com o vetor de dados vazio
+void liberarLista(Tipo_Lista *lista);
+
+// Retorna false só se a lista está vazia (sem dados)
+int verificarListaCheia(Tipo_Lista *lista);
+
+// Retorna false só se a lista tem algum dado
 int verificarListaVazia(Tipo_Lista *lista);
 
-int inserirInicioLista(Tipo_Lista *lista, Tipo_Lista *novoNode);
+// Insere o dado no ínicio da lista
+int inserirInicioLista(Tipo_Lista *lista, int valor);
 
-int inserirFinalLista(Tipo_Lista *lista, Tipo_Lista *novoNode);
+// Insere o dado no fim da lista
+int inserirFinalLista(Tipo_Lista *lista, int valor);
 
 int inserirMeioLista(Tipo_Lista *lista, Tipo_Lista *novoNode, int posicao);
 
