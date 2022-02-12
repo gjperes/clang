bbe6c824f4baecaf5a6db8d379c3e72b2a890f94
Gabriel <61798393+gjperes@users.noreply.github.com>
2022-02-12 11:41:23 -0300

    TODO: Compilar + Testar / implementadas funções restantes

--- estruturas/lista-linear-estatica/lista_sequencial.c
+++ estruturas/lista-linear-estatica/lista_sequencial.c
@@ -62,14 +62,111 @@ int inserirFinalLista(Tipo_Lista *lista, int valor) {
   return 1;
 }
 
-// TODO: Implementar a inserção no meio (ordenado)
+int inserirOrdenadoLista(Tipo_Lista *lista, int valor) {
+  
+  if(lista == NULL) return 0;
+  if(verificarListaCheia(lista)) return 0;
 
-// TODO: Precisa ser concluído
+  int i = 0;
+
+  // Percorre a lista comparando os valores com o novo dado a ser inserido, 
+  // Assim que o novo dado for maior que o número comparado, interrompe.
+  while(i < lista->fim && valor > lista->dados[i])
+    i++;
+  
+  // "Joga" pro lado os valores, pra inserir o novo dado no meio da lista
+  for(int j = (lista->fim - 1); j >= i; j--) {
+    lista->dados[j + 1] = lista->dados[j];
+  }
+
+  lista->dados[i] = valor;
+  lista->fim++;
+
+  return 1;
+}
+
 int removerInicioLista(Tipo_Lista *lista) {
   if(lista == NULL) return 0;
   if(verificarListaVazia) return 0;
 
-  lista->dados[0] = 0;
+  for(int i = 0; i < (lista->fim - 1); i++) {
+    lista->dados[i] = lista->dados[i + 1];
+  }
+
+  lista->fim--;
+
+  return 1;
+}
+
+int removerFinalLista(Tipo_Lista *lista) {
+  if(lista == NULL) return 0;
+  if(verificarListaVazia) return 0;
+
+  lista->dados[lista->fim] = 0;
+  lista->fim--;
+
+  return 1;
+}
+
+int removerMeioLista(Tipo_Lista *lista, int valor) {
+  if(lista == NULL) return 0;
+  if(verificarListaVazia) return 0;
+
+  int i = 0;
+
+  // Percorre a lista procurando o valor informado
+  while(i < lista->fim && valor != lista->dados[i])
+    i++;
+  
+  // Se percorreu a lista completamente e não encontrou o valor, não teve sucesso
+  if(i == lista->fim) return 0;
+
+  for(int k = i; k < (lista->fim - 1); k++) {
+    lista->dados[k] = lista->dados[k + 1];
+  }
+
+  lista->fim--;
+
+  return 1;
+}
+
+int consultarConteudoLista(Tipo_Lista *lista, int valor) {
+  if (lista == NULL || verificarListaVazia(lista)) 
+    return 0;
+
+  for(int i = 0; i <= lista->fim; i++) {
+    if(lista->dados[i] == valor)
+      return 1;
+  }
+
+  return 0;
+}
+
+int consultarPosicaoLista(Tipo_Lista *lista, int posicao) {
+  if (lista == NULL || verificarListaVazia(lista)) 
+    return 0;
+  
+  if(lista->fim < posicao || posicao < 0)
+    return 0;
+
+  return 1;
+}
+
+int imprimirLista(Tipo_Lista *lista) {
+  if(lista == NULL) {
+    printf("Lista não criada.\n");
+    return 0;
+  }
+
+  if(verificarListaVazia(lista)) {
+    printf("É uma lista vazia.\n");
+    return 0;
+  }
+
+  printf("Imprimindo lista...\n");
+  for(int i = 0; i <= lista->fim; i++) {
+    printf("%d\n", lista->dados[i]);
+  }
 
   return 1;
 }
--- estruturas/lista-linear-estatica/lista_sequencial.h
+++ estruturas/lista-linear-estatica/lista_sequencial.h
@@ -16,42 +16,74 @@ Tipo_Lista* criarLista();
  */
 void liberarLista(Tipo_Lista *lista);
 
-/** Verifica se a lista está cheia
+/** Verifica se a lista está cheia, se cheia -> TRUE
  * @param lista Lista para verificar
  * @return Lista Cheia ? True : False *Se NULL retorna -1
  */
 int verificarListaCheia(Tipo_Lista *lista);
 
-/** Verifica se a lista está vazia
+/** Verifica se a lista está vazia, se vazia -> TRUE
  * @param lista Lista para verificar
  * @return Lista Vazia ? True : False *Se NULL retorna -1
  */
 int verificarListaVazia(Tipo_Lista *lista);
 
 /** Insere o dado no início da lista (posição 0)
- * @param lista Lista para verificar
+ * @param lista Lista para inserir
  * @param valor Valor a ser inserido na lista
- * @return Sucesso na operação ? True : False
+ * @return Sucesso na inserção do valor ? True : False
  */
 int inserirInicioLista(Tipo_Lista *lista, int valor);
 
-/** Insere o dado no final da lista
- * @param lista Lista para verificar
+/** Insere o dado no fim da lista
+ * @param lista Lista para inserir
  * @param valor Valor a ser inserido na lista
- * @return Sucesso na operação ? True : False
+ * @return Sucesso na inserção do valor ? True : False
  */
 int inserirFinalLista(Tipo_Lista *lista, int valor);
 
-int inserirMeioLista(Tipo_Lista *lista, Tipo_Lista *novoNode, int posicao);
+/** Insere o dado de maneira ordenada na lista
+ * @param lista Lista para inserir
+ * @param valor Valor a ser inserido na lista
+ * @return Sucesso na inserção do valor ? True : False
+ */
+int inserirOrdenadoLista(Tipo_Lista *lista, int valor);
 
+/** Remove o primeiro elemento da lista, posição 0
+ * @param lista Lista que terá a remoção do elemento
+ * @return Sucesso na remoção ? True : False
+ */
 int removerInicioLista(Tipo_Lista *lista);
 
+/** Remove o último elemento da lista
+ * @param lista Lista que terá a remoção do elemento
+ * @return Sucesso na remoção ? True : False
+ */
 int removerFinalLista(Tipo_Lista *lista);
 
-int removerMeioLista(Tipo_Lista *lista, int posicao);
+/** Remove um elemento em específico da lista, se existente
+ * @param lista Lista que terá a remoção do elemento
+ * @param valor Valor que deverá ser removido se existente na lista
+ * @return Sucesso na remoção ? True : False
+ */
+int removerMeioLista(Tipo_Lista *lista, int valor);
 
-int consultarConteudoLista(Tipo_Lista *lista, int conteudo);
+/** Verifica se um determinado valor existe na lista
+ * @param lista Lista para verificação
+ * @param valor Valor a ser identificado na lista
+ * @return Encontrou o elemento ? True : False
+ */
+int consultarConteudoLista(Tipo_Lista *lista, int valor);
 
+/** Verifica se a lista é de tamanho igual ou maior que a posição indicada
+ * @param lista Lista para verificação
+ * @param posicao Posição a ser validade
+ * @return A posição existe na lista ? True : False
+ */
 int consultarPosicaoLista(Tipo_Lista *lista, int posicao);
 
+/** Imprime no console a lista
+ * @param lista Lista a ser impressa
+ * @return A lista foi impressa com sucesso ? True : False
+ */
 int imprimirLista(Tipo_Lista *lista);
